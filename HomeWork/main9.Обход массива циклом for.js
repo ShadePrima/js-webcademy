ee7a8fade144563043eb1f1b4ac6ef62d8e781ce
ShadePrima <aleksandrag1984@gmail.com>
2022-04-03 10:19:34 +0300

    for

---
+++ HomeWork/main9.Обход массива циклом for.js
@@ -0,0 +1,14 @@
+//================9.Обход массива циклом for=====================
+
+const autoBrands = ['Audi','BMW','Mazda','Toyota']
+
+// console.log(autoBrands[0])
+// console.log(autoBrands[1])
+// console.log(autoBrands[2])
+// console.log(autoBrands[3])
+// console.log(autoBrands[4])
+
+for (i = 0; i < autoBrands.length; i++) {
+    console.log(autoBrands[i])
+    // console.log(i)
+}
--- main.js
+++ main.js
@@ -1,3 +1,24 @@
-//================9.Обход массива циклом for=====================
+//=============10.Обход массива циклом for (of)====================
+
+// const autoBrands = ['Audi','BMW','Mazda','Toyota']
+
+// for (let brand of autoBrands) {
+//     console.log(brand)
+// } 
+
+
+//==============11.Обход массива методом forEach()=================
 
 const autoBrands = ['Audi','BMW','Mazda','Toyota']
+
+autoBrands.forEach(function(item, index) {
+    console.log(`${index} ${item}`)
+})
+
+// autoBrands.forEach(printBrand)
+
+// function printBrand (brand, i) {
+//     console.log(`${brand} => ${i}`)
+// }
+
+autoBrands.forEach((brand, i) => console.log(`${brand} => ${i}`) )
